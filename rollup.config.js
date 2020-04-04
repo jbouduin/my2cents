817059bf75119136fcbb37770522c80dd9f95ce0
Johan Bouduin <johan@der-e-coach.de>
2020-04-04 12:11:36 +0200

    allow for a production 'embed'

--- rollup.config.js
+++ rollup.config.js
@@ -5,46 +5,51 @@ import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 import jst from 'rollup-plugin-jst';
 
-const plugins = [
+var plugins = [
   jst({
     extensions: ['.html'],
     include: 'src/embed/**.html'
   }),
   commonjs(),
   resolve(),
-  buble(),
-  // uglify()
+  buble()
 ];
 
+var outputPath = 'build';
+if ((process.env.NODE_ENV || '').trim() === 'production') {
+  outputPath = 'dist/embed';
+  plugins.push(uglify());
+}
+
 export default [
-  {
-    input: 'src/embed/index.js',
-    output: {
-      file: 'build/embed.js',
-      format: 'iife'
-    },
-    plugins: plugins
-  }, {
-    input: 'src/embed/client.js',
-    output: {
-      file: 'build/client.js',
-      format: 'umd',
-      name: 'My2Cents'
-    },
-    plugins: plugins
-  },  {
-    input: 'src/embed/push.js',
-    output: {
-      file: 'build/push.js',
-      format: 'cjs'
-    },
-    plugins: plugins
-  }, {
-    input: 'src/embed/sw.js',
-    output: {
-      file: 'build/sw.js',
-      format: 'cjs'
-    },
-    plugins: plugins
-  }
-];
+    {
+      input: 'src/embed/index.js',
+      output: {
+        file: `${outputPath}/embed.js`,
+        format: 'iife'
+      },
+      plugins: plugins
+    }, {
+      input: 'src/embed/client.js',
+      output: {
+        file: `${outputPath}/client.js`,
+        format: 'umd',
+        name: 'My2Cents'
+      },
+      plugins: plugins
+    },  {
+      input: 'src/embed/push.js',
+      output: {
+        file: `${outputPath}/push.js`,
+        format: 'cjs'
+      },
+      plugins: plugins
+    }, {
+      input: 'src/embed/sw.js',
+      output: {
+        file: `${outputPath}/sw.js`,
+        format: 'cjs'
+      },
+      plugins: plugins
+    }
+  ];
