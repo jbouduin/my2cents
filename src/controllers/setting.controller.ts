2347238f21c5eac6b99cdbf6521c47cf5af4363f
Johan Bouduin <johan@der-e-coach.de>
2020-04-11 12:26:01 +0200

    Muting notifications was not saved

--- src/controllers/setting.controller.ts
+++ src/controllers/setting.controller.ts
@@ -41,6 +41,7 @@ export class SettingController implements ISettingController {
       if (!request.session.passport.user.administrator) {
         response.sendStatus(403);
       } else {
+        console.log(`setting ${request.params.key} to ${request.params.value}`);
         this.settingService.setSetting(request.params.key, request.params.value)
           .then(setting => response.send({ status: 'ok' }))
           .catch(err => {
